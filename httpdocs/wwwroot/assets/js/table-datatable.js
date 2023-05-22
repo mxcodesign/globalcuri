1f8acda1d9fa332f5cb1443b16e625fc04f8cdf8
mxcodesign <mxcodesign@gmail.com>
2023-05-21 18:03:18 -0600

    globalcuri

---
+++ httpdocs/wwwroot/assets/js/table-datatable.js
@@ -0,0 +1,24 @@
+$(function() {
+	"use strict";
+
+    $(document).ready(function() {
+        $('#example').DataTable();
+      } );
+
+
+      $(document).ready(function() {
+        var table = $('#example2').DataTable( {
+            lengthChange: false,
+            buttons: ['excel', 'pdf', 'print']
+        } );
+     
+        table.buttons().container()
+            .appendTo( '#example2_wrapper .col-md-6:eq(0)' );
+    } );
+
+
+    $(document).ready(function () {
+        $('#example3').DataTable();
+    });
+
+});
